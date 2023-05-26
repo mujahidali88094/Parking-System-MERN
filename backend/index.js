fa3a24509334dce87622aea6df8747217498be7d
mujahidali88094 <mujahidali88094@gmail.com>
2023-05-26 20:12:14 +0500

    mongo init

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,38 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
+const router = require("./routes");
+
+dotenv.config();
 const app = express();
 
-const port = 8000;
+// mongo databse connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
+// middlewares
+app.use(express.json());
+
+// routes
+app.use('/api', router);
+app.get("/api", (req, res) => {
+  res.send("API for Parking System in MERN!");
+});
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+//start listening to the server
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
